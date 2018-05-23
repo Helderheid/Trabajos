f99ea7ebb9b26be9efc11c830360bb8daea95d32
Helderheid <37373454+Helderheid@users.noreply.github.com>
2018-05-23 09:43:48 -0500

    Update ArrgloDinamicoInt.cpp

--- ArrgloDinamicoInt.cpp
+++ ArrgloDinamicoInt.cpp
@@ -35,4 +35,20 @@ void ArrgloDinamicoInt::vaciar(int size, int arr[]){
         data=0;
         }
 
+void ArrgloDinamicoInt::ordenar(int size, int arr[]){
+        int aux=0;
+          for(int i=0;i<size;i++){
+        for (int j=0; j<size-1; j++){
+            if (arr[j]>arr[j+1]){
+                aux=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=aux;
+            }
+        }
+    }
+    for(int i=0;i<size;i++){
+    cout << arr[i] <<endl;
+    }
+}
+
 
